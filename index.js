a9482f4b03e52da0192eb3089737929b581f5041
John Sonnenschein <johns@joyent.com>
2013-10-01 15:45:40 -0700

    SSO-44 keyapi should be a lib rather than an api

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function KeyAPI(options) {
   if (! options.log )
     options.log = new Logger({
             name: 'keyapi',
-            level: 'debug',
+            level: 'trace',
             serializers: {
                     err: Logger.stdSerializers.err,
                     req: Logger.stdSerializers.req
@@ -25,17 +25,20 @@ function KeyAPI(options) {
   this.tokenizer = new crypt({keycache: this.keycache, log: this.log.child({'component': 'crypt'})}); 
 
 }
-module.exports = KeyAPI;
 
-KeyAPI.prototype.detoken = function(token) {
+KeyAPI.prototype.detoken = function(token, cb) {
   var self = this;
   (self.tokenizer.detokenize(token, function(obj, err) {
-    cb(obj, err);
+    cb(JSON.parse(obj), err);
   }));
 };
 
-KeyAPI.prototype.token = function(obj) {
+KeyAPI.prototype.token = function(obj, cb) {
+  var self = this;
+  obj = JSON.stringify(obj);
   self.tokenizer.tokenize(obj, function(tok, err) {
     cb(tok, err);
   });
 };
+
+module.exports = KeyAPI;
