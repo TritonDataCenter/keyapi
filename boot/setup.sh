419cbeb5e0fa31baf3008a4d665da702f149e982
Andres Rodriguez <andres@joyent.com>
2013-10-15 12:35:29 -0700

    HEAD-1365 standard log rotation

--- boot/setup.sh
+++ boot/setup.sh
@@ -10,7 +10,6 @@ set -o xtrace
 PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
 
 role=keyapi
-app_name=$role
 
 # SAPI manifests
 CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/smartdc/keyapi
@@ -29,6 +28,13 @@ chown -R nobody:nobody /opt/smartdc/keyapi
 # Add build/node/bin and node_modules/.bin to PATH
 echo "" >>/root/.profile
 echo "export PATH=\$PATH:/opt/smartdc/keyapi/build/node/bin:/opt/smartdc/keyapi/node_modules/.bin" >>/root/.profile
+
+# Log rotation.
+sdc_log_rotation_add amon-agent /var/svc/log/*amon-agent*.log 1g
+sdc_log_rotation_add config-agent /var/svc/log/*config-agent*.log 1g
+sdc_log_rotation_add registrar /var/svc/log/*registrar*.log 1g
+sdc_log_rotation_add keyapi /var/svc/log/*keyapi*.log 1g
+sdc_log_rotation_setup_end
 
 # All done, run boilerplate end-of-setup
 sdc_setup_complete
