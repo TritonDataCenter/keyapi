54f8e0099b60ae9210f40a50c1ee63093e93fe5c
Pedro P. Candel <kusorbox@gmail.com>
2014-08-27 13:37:18 +0200

    TOOLS-607: Added Open Source License

--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,14 @@
 /*
- * Copyright (c) 2014, Joyent, Inc. All rights reserved.
- *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
+/*
  * These tests assume that they're operating with a clean database -- no entries
  * for KEYAPI_DN. If you're not running these tests in production (don't!), then
  * use 'sdc-ldap s' and 'sdc-ldap del' to clean out any extraneous entries.
